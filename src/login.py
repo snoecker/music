0741685b2ccb04af7abd3cc47e87e8ce7cae3006
snoecker <pockets71@yahoo.com>
2015-02-26 22:30:29 -0500

    Actually commit basic login read. So i can not add be dumb and add my login/pwd

--- src/login.py
+++ src/login.py
@@ -1,16 +1,39 @@
+"""Library to read file to get login/pw for Google acct.  File is expected in this format:
+username
+password
+"""
+
+from collections import namedtuple
 from os.path import expanduser
-try:
-	home= expanduser("~")
-	file=open(home + "/acct")
+# from sys import argv
+import sys
+
+Glogin= namedtuple("GoogleLogin","GUserName GPassword")
+
+
+def readGoogleLogin(gfilename=""):
 
 	try:
-		username=file.readline().rstrip("\n")
-		password=file.readline().rstrip("\n")
-		print username+" "+password
-	finally:
-		file.close
+		if not gfilename:
+			home = expanduser("~")+"/acct"
+			gfile=open(home,"r")
+		else:
+			gfile=open(gfilename,"r")
+		
+	
+		try:
+			username=gfile.readline().rstrip("\n")
+			password=gfile.readline().rstrip("\n")
+			print username+" "+password
+			return Glogin(username,password)
+		finally:
+			file.close
+	
+	except IOError:
+		print ("file not found")
 
-except IOError:
-	print ("file not found")
-
-
+if __name__ == '__main__':
+	if len(sys.argv)>1 :
+		readGoogleLogin(sys.argv[1])
+	else:
+		pass
--- src/test.py
+++ src/test.py
@@ -7,18 +7,22 @@ Created on Jan 12, 2015
 if __name__ == '__main__':
     pass
 from gmusicapi import Mobileclient, Api
+import login
 
 api = Mobileclient()
-logged_in = api.login('email@gmail.com', 'password')
-# logged_in is True if login was successful
+GAcct=login.readGoogleLogin()
 
-if logged_in == True:
-    print "logged in"
+if GAcct:
+    logged_in = api.login(GAcct.GUserName, GAcct.GPassword)
+#     logged_in is True if login was successful
+
+    if logged_in == True:
+        print "logged in"
     
     song_list= api.get_all_songs(True)
     for songs in song_list:
         print "size of list" + str(len(songs))
     logged_out= api.logout()
-    print logged_out
+    print "logged out " + str(logged_out)
     
     
